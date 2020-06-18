8ebaf9e38a215d794461b868aa58bd91129be60b
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-06-18 12:16:33 +0900

    TableViewのセットアップ .gitignore作成

--- Swift5 QiitaAPI/ViewController.swift
+++ Swift5 QiitaAPI/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let cellId = "cellId"
+    
+    let tableView: UITableView = {
+        let tv = UITableView()
+        return tv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        tableView.frame.size = view.frame.size
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        navigationItem.title = "Qiitaの記事"
+        
     }
-
-
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .darkGray
+        return cell
+    }
+    
+      
+      
+      
+  
+  }
 
